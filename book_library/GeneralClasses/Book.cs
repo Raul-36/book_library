e6b7e09d9313bfc4fa836cccfe79554fd31a6df3
Rau-36 <raul363636r36d@gmail.com>
2023-11-20 17:30:29 +0400

    Project start

---
+++ book_library/GeneralClasses/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GeneralClasses;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public int? UserId { get; set; }
+
+    [JsonIgnore]
+    public User? User { get; set; }
+}
+
---
+++ book_library/GeneralClasses/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GeneralClasses;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public int? BookId { get; set; }
+
+    [JsonIgnore]
+    public Book? Book { get; set; } 
+}
