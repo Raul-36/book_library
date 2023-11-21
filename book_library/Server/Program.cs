599d4256c37f0690fec2871b53cebfe37e405153
Rau-36 <raul363636r36d@gmail.com>
2023-11-21 16:13:58 +0400

    Add HttpListener

--- book_library/Server/Program.cs
+++ book_library/Server/Program.cs
@@ -1,3 +1,14 @@
 ﻿using Server.Data;
+using System.Net;
 
 LibraryDbContext libraryDbContext = new LibraryDbContext();
+HttpListener listener = new HttpListener();
+const int port = 3636;
+listener.Prefixes.Add($"http://localhost:{port}/");
+listener.Start();
+while ( true)
+{
+    HttpListenerContext context = listener.GetContext();
+    var re= context.Request.RawUrl;
+  
+}
