2a8541693abc77ba379fafc6a5b0d19aa1bc8e5f
Rau-36 <raul363636r36d@gmail.com>
2023-11-23 02:04:20 +0400

    Add SQLRepositories

---
+++ book_library/Server/SQLRepositories/Base/IBooksSQLRepository.cs
@@ -0,0 +1,17 @@
+﻿using GeneralClasses;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.SQLRepositories.Base;
+
+public interface IBookssSQLRepository
+{
+    public IEnumerable<Book> GetAll();
+    public IEnumerable<Book> GetAll(IEnumerable<Predicate<Book>> predicates);
+    public void Update(Book book);
+    public Book GetById(int id);
+}
---
+++ book_library/Server/SQLRepositories/UsersEFRepository.cs
@@ -0,0 +1,45 @@
+﻿using GeneralClasses;
+using Server.Data;
+using Server.SQLRepositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.SQLRepositories;
+public class UsersEFRepository : IUsersSQLRepository
+{
+    private LibraryDbContext context;
+    public UsersEFRepository(LibraryDbContext context)
+    {
+        this.context = context;
+    }
+    public User Create(User user)
+    {
+       context.Users.Add(user);
+        context.SaveChanges();
+        return user;
+    }
+
+    public void Delete(int id)
+    {
+        User userToDelete = context.Users.FirstOrDefault(u => u.Id == id);
+
+        if (userToDelete != null) { 
+            context.Users.Remove(userToDelete);
+            context.SaveChanges();
+        }
+    }
+
+    public User? GetById(int id)
+    {
+       return context.Users.FirstOrDefault(u => id == u.Id);
+    }
+
+    public void Update(User user)
+    {
+       context.Update(user);
+        context.SaveChanges();
+    }
+}
